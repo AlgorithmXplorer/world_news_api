af71c8c2471366039fd19e05d0812d63d32224eb
Mustafa <bulutmustafa.0134@gmail.com>
2025-01-01 01:45:27 +0300

    The function to retrieve user choices has been written in the panel class

--- main.py
+++ main.py
@@ -68,20 +68,64 @@ class news_writter:
                 break
 
         
-x = news_repo()
-x.language = "tr"
-x.country = "tr"
-x.categories_news_provider("politics")
-a = news_writter(x.list_of_news)
-a.pages()
+# x = news_repo()
+# x.language = "en"
+# x.country = "us"
+# x.categories_news_provider("politics")
+# a = news_writter(x.list_of_news)
+# a.pages()
 class panel:
-
     def __init__(self):
-        self.categories = ["Breaking News","Politics News","Technology News","Sports News","Business News","Science News","Culture News", "Entertainment News"]
-        self.repo = news_repo
+        self.categories = ["Breaking News","Politics News","Technology News","Sports News","Business News","Science News","Culture News", "Entertainment News","news search (title and or News subject)"]
+        self.repo = news_repo()
+        self.choice = "e"
+    
+    def getting_operation(self):
+        print(f"filtrs ; language:{self.repo.language} // country:{self.repo.country} // date:{self.repo.date} // authors:{self.repo.author}")
+        print("categories:")
+        for index,i in enumerate(self.categories,1):
+            print(f"\t{index}- get {i}")
+        numbers = list(range(1,10))
+        str_func = lambda x: str(x)
+        new_numbers = list(map(str_func, numbers))
+        while True:
+            try:
+                choice = input("choice(filters:f / categories:(numbers) / exit:e): ").lower().strip()
+                if choice not in  new_numbers or choice != "f" or choice != "e" :
+                    raise ValueError("please fill in the space correctly")
+            except ValueError:
+                pass
+            else:
+                break
+        self.choice = choice
+    def main(self):
+        #* burası chice attribute'una göre çalıştıırlcak fonksiyonların yeri
+        pass
+    def set_filters(self):
+        pass
+    def set_populer_news(self):
+        pass
+    def set_Technology_News(self):
+        pass
+    def set_(self):
+        pass
+    def set_Politics(self):
+        pass
+    def set_entertainment(self):
+        pass
+    def set_culture(self):
+        pass
+    def set_science(self):
+        pass
+    def set_business(self):
+        pass
+    def set_Sports_News(self):
+        pass
+    def search_news(self):
+        pass
 
-
-
+#todo panelde kullanıcının girdiği işleme göre main içinde fonksiyon çalıştırılıcak. 
+#todo her haberin kendi bir sistemi olduğu için her biri farklı fonksiyon
 
 
 
